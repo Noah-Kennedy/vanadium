953c247b5834c5c759e1ca1b378d3543b940afae
noah <nomaxx117@gmail.com>
2020-12-07 23:56:57 -0600

    Rayon for band writing

--- src/bin_formats/mat/mod.rs
+++ src/bin_formats/mat/mod.rs
@@ -2,7 +2,7 @@ use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 use std::thread;
 
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle, ProgressIterator};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 
 pub use color_maps::*;
 pub use conversion::*;
@@ -79,7 +79,7 @@ impl<C1, I1> Mat<C1, f32, I1>
     pub unsafe fn pca<C2>(&self, other: &mut Mat<C2, f32, Bsq>, kept_bands: u64)
         where C2: DerefMut<Target=[u8]> + Send + Sync
     {
-        let FileDims { bands, samples, lines} = self.inner.size();
+        let FileDims { bands, samples, lines } = self.inner.size();
 
         let sty = ProgressStyle::default_bar()
             .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} [{eta_precise}] {msg}")
@@ -125,30 +125,48 @@ impl<C1, I1> Mat<C1, f32, I1>
         status_bar.enable_steady_tick(200);
         status_bar.set_message("Writes");
 
-        eigen.eigenvectors.column_iter()
-            .zip(0..kept_bands)
-            .for_each(|(col, b1)| {
-                for l in 0..lines {
-                    for s in 0..samples {
-                        let read: Vec<f32> = (0..bands.len())
-                            .map(|b2| self.index.get_idx(l, s, b2))
-                            .map(|read_idx| r_ptr.0.add(read_idx).read_volatile())
-                            .collect();
+        let r_ptr = r_ptr.clone();
+        let w_ptr = w_ptr.clone();
+        let status_bar_c = status_bar.clone();
 
-                        let w_val: f32 = read.iter().zip(col.iter())
-                            .enumerate()
-                            .map(|(b2, (d, s))| (((*d) * (*s)) - means[b2]) / std_devs[b2])
-                            .sum();
+        rayon::scope(move |s| {
+            (0..kept_bands)
+                .into_iter()
+                .for_each(|b1| {
+                    let eig = eigen.eigenvectors.clone();
+                    let r_ptr = r_ptr.clone();
+                    let w_ptr = w_ptr.clone();
+                    let band_len = bands.len();
+                    let means = means.clone();
+                    let std_devs = std_devs.clone();
+                    let status_bar = status_bar.clone();
+                    let o_index = other.index.clone();
+                    s.spawn(move |_| {
+                        let col = eig.column(b1 as usize);
 
-                        let w_idx = other.index.get_idx(l, s, b1 as usize);
-                        w_ptr.0.add(w_idx).write_volatile(w_val);
-                    }
-                }
+                        for l in 0..lines {
+                            for s in 0..samples {
+                                let read: Vec<f32> = (0..band_len)
+                                    .map(|b2| self.index.get_idx(l, s, b2))
+                                    .map(|read_idx| r_ptr.0.add(read_idx).read_volatile())
+                                    .collect();
 
-                status_bar.inc(1);
-            });
+                                let w_val: f32 = read.iter().zip(col.iter())
+                                    .enumerate()
+                                    .map(|(b2, (d, s))| (((*d) * (*s)) - means[b2]) / std_devs[b2])
+                                    .sum();
 
-        status_bar.finish();
+                                let w_idx = o_index.get_idx(l, s, b1 as usize);
+                                w_ptr.0.add(w_idx).write_volatile(w_val);
+                            }
+                        }
+
+                        status_bar.inc(1);
+                    })
+                });
+        });
+
+        status_bar_c.finish();
 
         stages_bar.inc(1);
 
