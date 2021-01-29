f72034a1b7728c1b827c6ebfb2a5fc45b4ed274e
noah <nomaxx117@gmail.com>
2021-01-29 02:22:43 -0600

    Updated benchmarks script

--- benchmark.sh
+++ benchmark.sh
@@ -1,50 +1,40 @@
 #!/usr/bin/env bash
 
-COLOR='hyperspectra-cli color -i data/raw/unnormalized/unnorm.{type} -n data/raw/unnormalized/unnorm.{type}.hdr'
+PCA='hyperspectra-cli pca -i /data/bench-data/small.{type} -h /data/bench-data/small.{type}.hdr -o /data/small.pca.{type} -d 3 --min 0.0 --max 1.0'
 
-RGB='-o rgb.png -m 0 0 0 -x 0.5 0.5 1 -b 1 3 4 -c rgb'
-COOL='-o cool.png -m 0 -x 1 -b 3 -c coolwarm'
-GREY='-o grey.png -m 0 -x 1 -b 3 -c grey'
+CONVERT_IN='hyperspectra-cli convert -i /data/bench-data/small.{type} -n /data/bench-data/small.{type}.hdr'
 
-CONVERT_IN='hyperspectra-cli convert -i data/raw/unnormalized/unnorm.{type} -n data/raw/unnormalized/unnorm.{type}.hdr'
-
-CONVERT_OUT_BIP='-o out.bip -t bip'
-CONVERT_OUT_BIL='-o out.bil -t bil'
-CONVERT_OUT_BSQ='-o out.bsq -t bsq'
+CONVERT_OUT_BIP='-o /data/out.bip -t bip'
+CONVERT_OUT_BIL='-o /data/out.bil -t bil'
+CONVERT_OUT_BSQ='-o /data/out.bsq -t bsq'
 
 time hyperfine --warmup=2 \
   --prepare 'rm *.png *.bil *.bip *.bsq || true' \
   --export-markdown BENCHMARKS_CONVERT_WARM.md \
-  -L type bip,bil,bsq \
+  -L type bip,bsq \
   "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BIL" \
   "$CONVERT_IN $CONVERT_OUT_BSQ"
 
 time hyperfine --warmup=2 \
   --prepare 'rm *.png *.bil *.bip *.bsq || true' \
-  --export-markdown BENCHMARKS_COLOR_WARM.md \
-  -L type bip,bil,bsq \
-  "$COLOR $RGB" \
-  "$COLOR $COOL" \
-  "$COLOR $GREY"
+  --export-markdown BENCHMARKS_PCA_WARM.md \
+  -L type bip,bsq \
+  "$PCA"
 
 time hyperfine --warmup=2 \
   --prepare 'rm *.png *.bil *.bip *.bsq || true; sync; echo 3 | tee /proc/sys/vm/drop_caches' \
   --export-markdown BENCHMARKS_CONVERT_COLD.md \
-  -L type bip,bil,bsq \
+  -L type bip,bsq \
   "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BIL" \
   "$CONVERT_IN $CONVERT_OUT_BSQ"
 
 time hyperfine --warmup=2 \
   --prepare 'rm *.png *.bil *.bip *.bsq || true; sync; echo 3 | tee /proc/sys/vm/drop_caches' \
-  --export-markdown BENCHMARKS_COLOR_COLD.md \
-  -L type bip,bil,bsq \
-  "$COLOR $RGB" \
-  "$COLOR $COOL" \
-  "$COLOR $GREY"
+  --export-markdown BENCHMARKS_PCA_COLD.md \
+  -L type bip,bsq \
+  "$PCA"
 
-rm *.png *.bil *.bip *.bsq || true
+rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq || true
 
 chown noah *.md
 chmod 755 *.md
