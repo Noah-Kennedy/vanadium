53ab739e2d7953d67f298916a1554fa89f7a6360
noah <nomaxx117@gmail.com>
2021-01-29 20:44:47 -0600

    Added medium sized benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -1,43 +1,75 @@
 #!/usr/bin/env bash
 
-PCA='hyperspectra-cli pca --csv -i /data/bench-data/small.{type} -h /data/bench-data/small.{type}.hdr -o /data/small.pca.{type} -d 3 --min 0.0 --max 1.0'
+PCA_SMALL='hyperspectra-cli pca --csv -i /data/bench-data/small.{type} -h /data/bench-data/small.{type}.hdr -o /data/pca.{type} -d 3 --min 0.0 --max 1.0'
+PCA_MED='hyperspectra-cli pca --csv -i /data/bench-data/medium.{type} -h /data/bench-data/medium.{type}.hdr -o /data/pca.{type} -d 3 --min 0.0 --max 1.0'
 
 WARMUP='rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true'
 COLD_UP='rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true; sync; echo 3 | tee /proc/sys/vm/drop_caches'
 
-CONVERT_IN='hyperspectra-cli convert -i /data/bench-data/small.{type} -n /data/bench-data/small.{type}.hdr'
+CONVERT_IN_SMALL='hyperspectra-cli convert -i /data/bench-data/small.{type} -n /data/bench-data/small.{type}.hdr'
+CONVERT_IN_MED='hyperspectra-cli convert -i /data/bench-data/medium.{type} -n /data/bench-data/medium.{type}.hdr'
 
 CONVERT_OUT_BIP='-o /data/out.bip -t bip'
 CONVERT_OUT_BIL='-o /data/out.bil -t bil'
 CONVERT_OUT_BSQ='-o /data/out.bsq -t bsq'
 
-time hyperfine --warmup=2 \
-  --prepare "$WARMUP" \
-  --export-markdown benchmark-results/spectral/BENCHMARKS_CONVERT_WARM.md \
-  -L type bip,bsq \
-  "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BSQ"
+# small
 
 time hyperfine --warmup=2 \
   --prepare "$WARMUP" \
-  --export-markdown benchmark-results/spectral/BENCHMARKS_PCA_WARM.md \
+  --export-markdown benchmark-results/spectral/small/BENCHMARKS_CONVERT_WARM.md \
   -L type bip,bsq \
-  "$PCA"
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
+
+time hyperfine --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/spectral/small/BENCHMARKS_PCA_WARM.md \
+  -L type bip,bsq \
+  "$PCA_SMALL"
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/spectral/BENCHMARKS_CONVERT_COLD.md \
+  --export-markdown benchmark-results/spectral/small/BENCHMARKS_CONVERT_COLD.md \
   -L type bip,bsq \
-  "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BSQ"
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/spectral/BENCHMARKS_PCA_COLD.md \
+  --export-markdown benchmark-results/spectral/small/BENCHMARKS_PCA_COLD.md \
   -L type bip,bsq \
-  "$PCA"
+  "$PCA_SMALL"
+
+# medium
+
+time hyperfine --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/spectral/medium/BENCHMARKS_CONVERT_WARM.md \
+  -L type bip,bsq \
+  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+
+time hyperfine --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/spectral/medium/BENCHMARKS_PCA_WARM.md \
+  -L type bip,bsq \
+  "$PCA_MED"
+
+time hyperfine --warmup=2 \
+  --prepare "$COLD_UP" \
+  --export-markdown benchmark-results/spectral/medium/BENCHMARKS_CONVERT_COLD.md \
+  -L type bip,bsq \
+  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+
+time hyperfine --warmup=2 \
+  --prepare "$COLD_UP" \
+  --export-markdown benchmark-results/spectral/medium/BENCHMARKS_PCA_COLD.md \
+  -L type bip,bsq \
+  "$PCA_MED"
 
 rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true
 
-chown noah *.md
-chmod 755 *.md
+chown --recursive noah ./benchmark-results/spectral/
+chmod --recursive 755 ./benchmark-results/spectral/
--- siproc-benchmark.sh
+++ siproc-benchmark.sh
@@ -1,44 +1,77 @@
 #!/usr/bin/env bash
 
-PCA='bins/siproc /data/bench-data/siproc/small-{type} /data/small.{type}.pca.siproc.csv --pca'
-WARMUP='rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true'
-COLD_UP='rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true; sync; echo 3 | tee /proc/sys/vm/drop_caches'
+PCA_SMALL='bins/siproc /data/bench-data/siproc/small-{type} /data/small.{type}.pca.siproc.csv --pca'
+PCA_MED='bins/siproc /data/bench-data/siproc/medium-{type} /data/medium.{type}.csv --pca'
 
-CONVERT_IN='bins/siproc /data/bench-data/siproc/small-{type}'
+WARMUP='rm /data/*.png /data/*bil /data/*bip /data/*bsq /data/*.csv || true'
+COLD_UP='rm /data/*.png /data/*bil /data/*bip /data/*bsq /data/*.csv || true; sync; echo 3 | tee /proc/sys/vm/drop_caches'
+
+CONVERT_IN_SMALL='bins/siproc /data/bench-data/siproc/small-{type}'
+CONVERT_IN_MED='bins/siproc /data/bench-data/siproc/medium-{type}'
 
 CONVERT_OUT_BIP='data/siproc-convert.bip --convert bip'
 CONVERT_OUT_BIL='data/siproc-convert.bil --convert bil'
 CONVERT_OUT_BSQ='data/siproc-convert.bsq --convert bsq'
 
-time hyperfine -u second -i --warmup=2 \
-  --prepare "$WARMUP" \
-  --export-markdown benchmark-results/siproc/SIPROC_BENCHMARKS_CONVERT_WARM.md \
-  -L type bip,bsq,bil \
-  "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BIL" \
-  "$CONVERT_IN $CONVERT_OUT_BSQ"
+# small
 
 time hyperfine -u second -i --warmup=2 \
   --prepare "$WARMUP" \
-  --export-markdown benchmark-results/siproc/SIPROC_BENCHMARKS_PCA_WARM.md \
+  --export-markdown benchmark-results/siproc/small/BENCHMARKS_CONVERT_WARM.md \
   -L type bip,bsq,bil \
-  "$PCA"
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIL" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
+
+time hyperfine -u second -i --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/siproc/small/BENCHMARKS_PCA_WARM.md \
+  -L type bip,bsq,bil \
+  "$PCA_SMALL"
 
 time hyperfine -u second -i --warmup=2 \
   --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/siproc/SIPROC_BENCHMARKS_CONVERT_COLD.md \
+  --export-markdown benchmark-results/siproc/small/BENCHMARKS_CONVERT_COLD.md \
   -L type bip,bsq,bil \
-  "$CONVERT_IN $CONVERT_OUT_BIP" \
-  "$CONVERT_IN $CONVERT_OUT_BIL" \
-  "$CONVERT_IN $CONVERT_OUT_BSQ"
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BIL" \
+  "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
 
 time hyperfine -u second --warmup=2 \
   --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/siproc/SIPROC_BENCHMARKS_PCA_COLD.md \
+  --export-markdown benchmark-results/siproc/small/BENCHMARKS_PCA_COLD.md \
   -L type bip,bsq,bil \
-  "$PCA"
+  "$PCA_SMALL"
+
+# medium
+
+time hyperfine --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/siproc/medium/BENCHMARKS_CONVERT_WARM.md \
+  -L type bip,bsq \
+  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+
+time hyperfine --warmup=2 \
+  --prepare "$WARMUP" \
+  --export-markdown benchmark-results/siproc/medium/BENCHMARKS_PCA_WARM.md \
+  -L type bip,bsq \
+  "$PCA_MED"
+
+time hyperfine --warmup=2 \
+  --prepare "$COLD_UP" \
+  --export-markdown benchmark-results/siproc/medium/BENCHMARKS_CONVERT_COLD.md \
+  -L type bip,bsq \
+  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+
+time hyperfine --warmup=2 \
+  --prepare "$COLD_UP" \
+  --export-markdown benchmark-results/siproc/medium/BENCHMARKS_PCA_COLD.md \
+  -L type bip,bsq \
+  "$PCA_MED"
 
 rm /data/*.png /data/*.bil /data/*.bip /data/*.bsq /data/*.csv || true
 
-chown noah ./benchmark-results/siproc/*.md
-chmod 755 ./benchmark-results/siproc/*.md
+chown --recursive noah ./benchmark-results/siproc/
+chmod --recursive 755 ./benchmark-results/siproc/
