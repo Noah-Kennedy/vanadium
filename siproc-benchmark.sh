4a5b69bd553ce917df40215c16d4e03f7057abac
noah <nomaxx117@gmail.com>
2021-02-03 14:39:39 -0600

    Commented out medium bench temporarily

--- siproc-benchmark.sh
+++ siproc-benchmark.sh
@@ -65,47 +65,47 @@ chown --recursive noah ./benchmark-results/siproc/
 chmod --recursive 755 ./benchmark-results/siproc/
 
 # medium
-
-time hyperfine -u second -i --warmup=2 \
-  --prepare "$WARMUP" \
-  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_CONVERT_WARM.md \
-  -L type bip,bil,bsq \
-  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
-  "$CONVERT_IN_MED $CONVERT_OUT_BIL" \
-  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
-
-rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
-chown --recursive noah ./benchmark-results/siproc/
-chmod --recursive 755 ./benchmark-results/siproc/
-
-time hyperfine -u second -i --warmup=2 \
-  --prepare "$WARMUP" \
-  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_PCA_WARM.md \
-  -L type bip,bil,bsq \
-  "$PCA_MED"
-
-rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
-chown --recursive noah ./benchmark-results/siproc/
-chmod --recursive 755 ./benchmark-results/siproc/
-
-time hyperfine -u second -i --warmup=2 \
-  --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_CONVERT_COLD.md \
-  -L type bip,bil,bsq \
-  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
-  "$CONVERT_IN_MED $CONVERT_OUT_BIL" \
-  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
-
-rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
-chown --recursive noah ./benchmark-results/siproc/
-chmod --recursive 755 ./benchmark-results/siproc/
-
-time hyperfine -u second -i --warmup=2 \
-  --prepare "$COLD_UP" \
-  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_PCA_COLD.md \
-  -L type bip,bil,bsq \
-  "$PCA_MED"
-
-rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
-chown --recursive noah ./benchmark-results/siproc/
-chmod --recursive 755 ./benchmark-results/siproc/
+#
+#time hyperfine -u second -i --warmup=2 \
+#  --prepare "$WARMUP" \
+#  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_CONVERT_WARM.md \
+#  -L type bip,bil,bsq \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BIL" \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+#
+#rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
+#chown --recursive noah ./benchmark-results/siproc/
+#chmod --recursive 755 ./benchmark-results/siproc/
+#
+#time hyperfine -u second -i --warmup=2 \
+#  --prepare "$WARMUP" \
+#  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_PCA_WARM.md \
+#  -L type bip,bil,bsq \
+#  "$PCA_MED"
+#
+#rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
+#chown --recursive noah ./benchmark-results/siproc/
+#chmod --recursive 755 ./benchmark-results/siproc/
+#
+#time hyperfine -u second -i --warmup=2 \
+#  --prepare "$COLD_UP" \
+#  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_CONVERT_COLD.md \
+#  -L type bip,bil,bsq \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BIP" \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BIL" \
+#  "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
+#
+#rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
+#chown --recursive noah ./benchmark-results/siproc/
+#chmod --recursive 755 ./benchmark-results/siproc/
+#
+#time hyperfine -u second -i --warmup=2 \
+#  --prepare "$COLD_UP" \
+#  --export-markdown benchmark-results/"$DEVICE"/siproc/medium/BENCHMARKS_PCA_COLD.md \
+#  -L type bip,bil,bsq \
+#  "$PCA_MED"
+#
+#rm /data/*png /data/*bil /data/*bip /data/*bsq /data/*.csv || true
+#chown --recursive noah ./benchmark-results/siproc/
+#chmod --recursive 755 ./benchmark-results/siproc/
