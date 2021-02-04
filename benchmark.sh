a58a7f6dcd334bf00a6e31a39cfb08d06b3b575f
noah <nomaxx117@gmail.com>
2021-02-04 09:14:54 -0600

    fixed benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -4,18 +4,18 @@ DEVICE='laptop'
 IN_DIR='bench-data'
 OUT_DIR='.'
 
-PCA_SMALL="hyperspectra-cli pca --csv -i $IN_DIR/small.{type} -h $OUT_DIR/small.{type}.hdr -o $OUT_DIR/pca.{type} -d 3 --min 0.0 --max 1.0"
-PCA_MED="hyperspectra-cli pca --csv -i $IN_DIR/medium.{type} -h $OUT_DIR/medium.{type}.hdr -o $OUT_DIR/pca.{type} -d 3 --min 0.0 --max 1.0"
+PCA_SMALL="hyperspectra-cli pca --csv -i $IN_DIR/small-{type} -h $OUT_DIR/small-{type}.hdr -o $OUT_DIR/pca.{type} -d 3 --min 0.0 --max 1.0"
+PCA_MED="hyperspectra-cli pca --csv -i $IN_DIR/medium-{type} -h $OUT_DIR/medium-{type}.hdr -o $OUT_DIR/pca.{type} -d 3 --min 0.0 --max 1.0"
 
-WARMUP="rm $OUT_DIR/*.png $OUT_DIR/*.bil $OUT_DIR/*.bip $OUT_DIR/*.bsq $OUT_DIR/*.csv || true"
-COLD_UP="rm $OUT_DIR/*.png $OUT_DIR/*.bil $OUT_DIR/*.bip $OUT_DIR/*.bsq $OUT_DIR/*.csv || true; sync; echo 3 | tee /proc/sys/vm/drop_caches"
+WARMUP="rm $OUT_DIR/*.png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true"
+COLD_UP="rm $OUT_DIR/*.png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true; sync; echo 3 | tee /proc/sys/vm/drop_caches"
 
-CONVERT_IN_SMALL="hyperspectra-cli convert -i $IN_DIR/small.{type} -n $IN_DIR/small.{type}.hdr"
-CONVERT_IN_MED="hyperspectra-cli convert -i $IN_DIR/medium.{type} -n $IN_DIR/medium.{type}.hdr"
+CONVERT_IN_SMALL="hyperspectra-cli convert -i $IN_DIR/small-{type} -n $IN_DIR/small-{type}.hdr"
+CONVERT_IN_MED="hyperspectra-cli convert -i $IN_DIR/medium-{type} -n $IN_DIR/medium-{type}.hdr"
 
-CONVERT_OUT_BIP="-o $OUT_DIR/out.bip -t bip"
-CONVERT_OUT_BIL="-o $OUT_DIR/out.bil -t bil"
-CONVERT_OUT_BSQ="-o $OUT_DIR/out.bsq -t bsq"
+CONVERT_OUT_BIP="-o $OUT_DIR/out-bip -t bip"
+CONVERT_OUT_BIL="-o $OUT_DIR/out-bil -t bil"
+CONVERT_OUT_BSQ="-o $OUT_DIR/out-bsq -t bsq"
 
 # small
 
@@ -27,8 +27,8 @@ time hyperfine --warmup=2 \
   "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$WARMUP" \
@@ -37,8 +37,8 @@ time hyperfine --warmup=2 \
   "$PCA_SMALL"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
@@ -48,8 +48,8 @@ time hyperfine --warmup=2 \
   "$CONVERT_IN_SMALL $CONVERT_OUT_BSQ"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
@@ -58,8 +58,8 @@ time hyperfine --warmup=2 \
   "$PCA_SMALL"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 # medium
 
@@ -71,8 +71,8 @@ time hyperfine --warmup=2 \
   "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$WARMUP" \
@@ -81,8 +81,8 @@ time hyperfine --warmup=2 \
   "$PCA_MED"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
@@ -92,8 +92,8 @@ time hyperfine --warmup=2 \
   "$CONVERT_IN_MED $CONVERT_OUT_BSQ"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
 
 time hyperfine --warmup=2 \
   --prepare "$COLD_UP" \
@@ -102,5 +102,5 @@ time hyperfine --warmup=2 \
   "$PCA_MED"
 
 rm $OUT_DIR/*png $OUT_DIR/*bil $OUT_DIR/*bip $OUT_DIR/*bsq $OUT_DIR/*.csv || true
-chown --recursive noah ./benchmark-results/"$DEVICE"/siproc/
-chmod --recursive 755 ./benchmark-results/"$DEVICE"/siproc/
+chown --recursive noah ./benchmark-results/"$DEVICE"/spectral/
+chmod --recursive 755 ./benchmark-results/"$DEVICE"/spectral/
