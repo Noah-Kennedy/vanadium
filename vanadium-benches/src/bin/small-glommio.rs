d0bbcd319e03afb40a47af6eb900b19546ca6ae5
noah <nomaxx117@gmail.com>
2021-07-25 08:54:48 -0500

    Added TCMalloc to benchmarks.

--- vanadium-benches/src/bin/small-glommio.rs
+++ vanadium-benches/src/bin/small-glommio.rs
@@ -1,7 +1,12 @@
 extern crate openblas_src;
 
+use tcmalloc::TCMalloc;
+
 use vanadium_benches::{bench_covariance, small_header};
-use vanadium_core::ops::{get_image_f32, BackendSelector};
+use vanadium_core::ops::{BackendSelector, get_image_f32};
+
+#[global_allocator]
+static GLOBAL: TCMalloc = TCMalloc;
 
 fn main() {
     let mut image = get_image_f32(BackendSelector::Glommio, small_header()).unwrap();
