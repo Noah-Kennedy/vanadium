dd18f37ccb27add89fd9260e462d74e515aecdf0
noah <nomaxx117@gmail.com>
2021-08-07 11:56:56 -0500

    Readded benches

--- vanadium-benches/src/bin/big-means-glommio.rs
+++ vanadium-benches/src/bin/big-means-glommio.rs
@@ -2,8 +2,8 @@ extern crate openblas_src;
 
 use tcmalloc::TCMalloc;
 
-use vanadium_benches::{large_header, bench_means};
-use vanadium_io::bip::GlommioBip;
+use vanadium_benches::{bench_means, large_header};
+use vanadium_core::io::bip::GlommioBip;
 
 #[global_allocator]
 static GLOBAL: TCMalloc = TCMalloc;
